a0c4d786e172823eb43467736ae7295edd68a747
Thomas Dekeyser <thomas@interchain.io>
2022-09-27 21:56:09 +0200

    handle playing game

--- x/smaller/keeper/msg_server_play_game.go
+++ x/smaller/keeper/msg_server_play_game.go
@@ -2,16 +2,44 @@ package keeper
 
 import (
 	"context"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/tmsdkeys/smaller3/x/smaller/types"
 )
 
 func (k msgServer) PlayGame(goCtx context.Context, msg *types.MsgPlayGame) (*types.MsgPlayGameResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	systemInfo, found := k.Keeper.GetSystemInfo(ctx)
+	if !found {
+		panic("SystemInfo not found, cannot continue")
+	}
+	newIndex := strconv.FormatUint(systemInfo.NextId, 10)
 
-	return &types.MsgPlayGameResponse{}, nil
+	numPick, err := strconv.ParseUint(msg.NumPick, 10, 64)
+	if err != nil {
+		// TODO: change to correct error
+		return nil, sdkerrors.Wrapf(types.ErrInvalidLengthStoredGame, msg.NumPick)
+	}
+
+	win := numPick < 3
+
+	storedGame := types.StoredGame{
+		Index:    newIndex,
+		Player:   msg.Creator,
+		Win:      win,
+		SentToLB: false,
+	}
+
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	systemInfo.NextId++
+	k.Keeper.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgPlayGameResponse{
+		GameIndex: newIndex,
+		Win:       win,
+	}, nil
 }
